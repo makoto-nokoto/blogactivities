58a05245c37c37be280d509af42b114f0bb9379f
919im <919image@gmail.com>
2020-02-19 22:56:19 +0900

    add edit mode

--- activity/urls.py
+++ activity/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.activity_list, name='activity_list'),
+    path('activity/<int:pk>/', views.post_detail, name='post_detail'),
+    path('activity/new/', views.post_new, name='post_new'),
+    path('activity/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
